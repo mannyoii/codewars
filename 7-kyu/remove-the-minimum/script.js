2995d2d559bda0f2d28599eee699d6446e07ce9f
mannyoii <mannyoii@protonmail.com>
2023-02-22 23:45:56 +0530

    updated readme.md

--- 7-kyu/remove-the-minimum/script.js
+++ 7-kyu/remove-the-minimum/script.js
@@ -3,6 +3,6 @@ function removeSmallest(numbers) {
   const smallest = numbers.reduce((a, b) => Math.min(a, b));
   const index = numbers.indexOf(smallest);
   const newArr = numbers.slice(0, numbers.length);
-  const newNum = newArr.splice(index, 1);
+  newArr.splice(index, 1);
   return newArr;
 }
