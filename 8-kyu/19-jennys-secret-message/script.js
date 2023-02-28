eacdbb90526332046914f250693ebe953123c18e
mannyoii <mannyoii@protonmail.com>
2023-02-28 23:49:50 +0530

    refactor code

--- 8-kyu/19-jennys-secret-message/script.js
+++ 8-kyu/19-jennys-secret-message/script.js
@@ -1,5 +1,3 @@
 function greet(name) {
-  if (name === "Johnny") {
-    return "Hello, my love!";
-  } else return "Hello, " + name + "!";
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
 }
